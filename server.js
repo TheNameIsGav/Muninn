e6fd10be21a860780038374a27a411316c0a660a
TheNameIsGav <spqr.dominus@gmail.com>
2022-04-04 13:25:37 -0500

    Added procfile

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 
 //NodeJs Server Setup
 var http = require('http');
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Deployed To Heroku!');
-}).listen(8080);
+}).listen(port);
+
 
 //Mongo Database Stuff
 var {MongoClient} = require('mongodb');
