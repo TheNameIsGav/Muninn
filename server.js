a6cccb87d33a09091cd1f1ec128d6e7fc7c86f09
TheNameIsGav <spqr.dominus@gmail.com>
2022-04-06 11:45:34 -0500

    Working with environment secrets in Github

--- server.js
+++ server.js
@@ -13,38 +13,3 @@ let app = http.createServer(function (req, res) {
 })
 
 app.listen(port);
-
-
-//Mongo Database Stuff
-var {MongoClient} = require('mongodb');
-require('dotenv').config();
-
-async function main(){
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-  const uri = "mongodb+srv://" + process.env.USER_NAME + ":" + process.env.USER_PASSWORD + "@muninn.m3vbg.mongodb.net/test?retryWrites=true&w=majority";
-  const client = new MongoClient(uri)
-  
-  
-  try {
-    await client.connect();
-    await listDatabases(client);
-  } catch (e) {
-    console.error(e);
-  }
-  
-  finally {
-    await client.close();
-  }
-}
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases: ");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-}
-
-main().catch(console.error);
