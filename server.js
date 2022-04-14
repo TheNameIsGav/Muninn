c3af57700e8c336fd8b4783f22ae23df7879c68f
TheNameIsGav <spqr.dominus@gmail.com>
2022-04-14 12:26:06 -0500

    Mongoose and Server connection success affirmation

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const express = require('express');
 const path = require('path');
+require('./Database/database.js')
+const mongoose = require('mongoose');
 
 //NodeJS Server Setup
 const app = express();
@@ -17,7 +19,14 @@ app.get('/', (req, res) => {
   res.render("index");
 });
 
+mongoose.connection.on('connected', () => {
+  console.log('connected');
+})
 
+mongoose.connection.on('error', function (err) {
+  console.log("Failed to connect with error " + err);
+  core.setFailed("Failed connection " + err);
+})
 
 app.listen(port, ()=>{
   console.log("listening at http://localhost:/" + port)
