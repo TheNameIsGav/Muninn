8cfe59f9c35b8223481dbfd4b37a7523da02db12
Youngone Lee <youngone.lee@accenture.com>
2022-04-22 08:04:41 -0500

    frontend: working search bar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,42 +4,31 @@ import { MenuItems } from './MenuItems'
 import './Navbar.css'
 
 class Navbar extends Component {
-    // state = {
-    //     value: ''
-    // };
-    
-    // getValue = (event) => {
-    //     console.log('Event: ', event.target.value);
-    
-    //     this.setState({ value: event.target.value});
-    // }
 
-    // handleSubmit = (event) => {
-    //     event.preventDefault(); //stops reloading the page and losing data
-    //     const value = this.state.value;
+    state = {
+        value: ''
+    }
 
-    //     console.log('Value on Submit', value);
+    getValue = (event) => {
+        this.setState({ value: event.target.value });
+    }
 
-    // }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const value = this.state.value;
+        console.log("value on submit: ", value);
+    }
 
     render() {
-        console.log('state: ', this.state);
-
-        getValue = () => {
-
-        }
-
         return(
             <div>
                 <div className='NavbarItems'>
                     <img className='navbar-logo' alt= "our Logo" src = "muninnLogo.png"/>
                     <form action="">
-                        <div>
                             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-                            <input type="text" onChange={ this.getValue}/>
-                            <button><i class="fa fa-search"></i></button>
-                            </form>
-                        </div>
+                            <input type="text" onChange={ this.getValue } />
+                            <button onClick={ this.handleSubmit }><i class="fa fa-search"></i></button>
                     </form>
                         {MenuItems.map((item, index) => {
                             return (
