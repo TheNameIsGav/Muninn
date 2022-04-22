948aed8754fad453c4495c541dc67d594cb75810
Youngone Lee <youngone.lee@accenture.com>
2022-04-22 07:07:42 -0500

    frontend: non-working code rn

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,22 +1,46 @@
 import React, {Component} from 'react';
 import { Button } from '../Button';
-import { SearchBar } from '../Search';
 import { MenuItems } from './MenuItems'
 import './Navbar.css'
 
 class Navbar extends Component {
-    state = { clicked: false }
+    // state = {
+    //     value: ''
+    // };
+    
+    // getValue = (event) => {
+    //     console.log('Event: ', event.target.value);
+    
+    //     this.setState({ value: event.target.value});
+    // }
 
-    handleClick = () => {
-        this.setState({clicked: !this.state.clicked})
-    }
+    // handleSubmit = (event) => {
+    //     event.preventDefault(); //stops reloading the page and losing data
+    //     const value = this.state.value;
+
+    //     console.log('Value on Submit', value);
+
+    // }
 
     render() {
+        console.log('state: ', this.state);
+
+        getValue = () => {
+
+        }
+
         return(
             <div>
                 <div className='NavbarItems'>
                     <img className='navbar-logo' alt= "our Logo" src = "muninnLogo.png"/>
-                    <SearchBar> </SearchBar>
+                    <form action="">
+                        <div>
+                            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+                            <input type="text" onChange={ this.getValue}/>
+                            <button><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>
+                    </form>
                         {MenuItems.map((item, index) => {
                             return (
                                 <React.Fragment>
