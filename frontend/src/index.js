7b7e61df9c059f2da61e0fb59671afe27660950d
Youngone Lee <youngone.lee@accenture.com>
2022-04-24 12:47:59 -0500

    frontend: working on linking the two again

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './components/Navbar/Navbar';
+
+var request = require("request"); //needed for post request
 
 ReactDOM.render(<App/>,
   document.getElementById('root')
 );
+
+var searchInformation = { searchTerm: Navbar.value}
+
+var postConfig = {
+  url: 'http://localhost:8000/search_game',
+  form: searchInformation
+}
+
+var postSuccessHandler = function (err, httpResponse, body) {
+  console.log('JSON response from the server: ' + body)
+}
+
+request.post(postConfig, postSuccessHandler);
+
+
+// request({url:"http://localhost:8000/search_game/just dance"}, function(err, response, body) {
+//   if(err) { console.log(err); return; }
+//   console.log("Get response code " + response.statusCode + " with text: " + response.body);
+// });
+
