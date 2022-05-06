3f2c42d500538e903f446ff0edcc89998e997eae
TheNameIsGav <spqr.dominus@gmail.com>
2022-05-06 08:40:04 -0500

    Fixed bad display error

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
     }
 
     componentDidMount() {
-        this.setState({value: "Doom"});
+        this.state.value = "Doom";
         this.searchItem()
     }
 
