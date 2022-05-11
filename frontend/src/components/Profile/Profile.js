b0c5f2195aae70e562e82845f3bf2048c4899149
Brendan Potter <brendanpotter00@gmail.com>
2022-05-11 14:03:00 -0500

    revert search

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -13,23 +13,33 @@ function Profile() {
   const handleSubmit = (e) => {
     e.preventDefault();
     fetch("/api/search_game/" + input)
-      .then((res) => {
-        try {
-          res.text();
-        } catch (e) {
-          alert("No game found");
-        }
-      })
+      .then((res) => res.text())
       .then((text) => {
         try {
           var game = JSON.parse(text);
           navigate("/search", { replace: true, state: { game } });
         } catch (e) {
-          //setInput("")
-          alert("No game found!");
+          setInput("");
+          alert("No game found");
         }
       });
   };
+
+  //   const searchItem = (e) => {
+  //     e.preventDefault();
+  //     console.log("Search for item after enter works");
+
+  //     fetch("/api/search_game/" + searchTerm)
+  //       .then((res) => res.text())
+  //       .then((text) => {
+  //         try {
+  //           var val = JSON.parse(text);
+  //           setGame(val);
+  //         } catch (e) {
+  //           alert("That game appears to not exist");
+  //         }
+  //       });
+  //   };
 
   useEffect(() => {
     if (location.state !== null) {
